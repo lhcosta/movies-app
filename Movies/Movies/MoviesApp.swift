9cf8602dbbd67a8133ef83cf6e0efb1ec1270e1a
lhcosta <lucashpc91@gmail.com>
2025-08-30 01:17:03 -0300

    Add movies project

---
+++ Movies/Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Lucas Costa on 30/08/25.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Movies/MoviesTests/MoviesTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesTests.swift
+//  MoviesTests
+//
+//  Created by Lucas Costa on 30/08/25.
+//
+
+import Testing
+@testable import Movies
+
+struct MoviesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
