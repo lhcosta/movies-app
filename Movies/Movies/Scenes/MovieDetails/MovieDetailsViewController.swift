1fe7bb6850a56ec5d6f2386e83a73a3831b1af50
lhcosta <lucashpc91@gmail.com>
2025-08-30 14:15:22 -0300

    Adicionando tela de detalhes de um filme

---
+++ Movies/Movies/Scenes/MovieDetails/MovieDetailsViewController.swift
@@ -0,0 +1,64 @@
+//
+//  MovieDetailsViewController.swift
+//  Movies
+//
+//  Created by Lucas Costa on 30/08/25.
+//
+
+import UIKit
+
+final class MovieDetailsViewController: UIViewController {
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 17)
+        label.numberOfLines = 0
+        return label
+    }()
+
+    private let dateLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 15, weight: .light)
+        label.textColor = .secondaryLabel
+        return label
+    }()
+
+    private lazy var contentStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [descriptionLabel, dateLabel])
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+
+    // MARK: - Ciclo de Vida da View
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+
+    private func setupView() {
+        view.backgroundColor = .systemBackground
+        view.addSubview(imageView)
+        view.addSubview(contentStackView)
+
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            imageView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.4),
+
+            contentStackView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20),
+            contentStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            contentStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+        ])
+    }
+}
