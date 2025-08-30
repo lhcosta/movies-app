d3064646383749effce32562493dcc69fd0cc939
lhcosta <lucashpc91@gmail.com>
2025-08-30 14:14:49 -0300

    Criando PosterView que representa um Filme

---
+++ Movies/Movies/Scenes/MoviesList/Views/PosterView.swift
@@ -0,0 +1,45 @@
+//
+//  PosterView.swift
+//  Movies
+//
+//  Created by Lucas Costa on 30/08/25.
+//
+import SwiftUI
+
+struct PosterDetails {
+    let url: URL?
+    let title: String
+}
+
+struct PosterView: View {
+    let details: PosterDetails
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            AsyncImage(url: details.url) { result in
+                switch result {
+                case .success(let image):
+                    image
+                        .resizable()
+                        .scaledToFit()
+                default:
+                    Image(systemName: "star")
+                }
+            }
+            
+            Text(details.title)
+                .font(.callout)
+                .foregroundColor(.primary)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    PosterView(
+        details: .init(
+            url: .init(string: "https://midias.imagemfilmes.com.br/capas/d536dc70-5e53-46d0-82ce-31a314f542c3_m.jpg?2025-07-31T13:42:56.347024"),
+            title: "A Guerra dos Mundos"
+        )
+    )
+}
