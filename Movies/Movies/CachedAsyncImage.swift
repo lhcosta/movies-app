b8af12349516625510059a5de03efb9f8415b4e3
lhcosta <lucashpc91@gmail.com>
2025-08-31 11:35:34 -0300

    Adicionando view para realizar cache de imagens

--- Movies/Movies/CachedAsyncImage.swift
+++ Movies/Movies/CachedAsyncImage.swift
@@ -7,12 +7,39 @@
 
 import SwiftUI
 
-struct CachedAsyncImage: View {
+struct CachedAsyncImage<Content: View>: View {
+    private let url: URL?
+    private let content: (AsyncImagePhase) -> Content
+    
+    init(
+        url: URL?,
+        @ViewBuilder content: @escaping (AsyncImagePhase) -> Content
+    ) {
+        self.url = url
+        self.content = content
+    }
+    
+    @State
+    private var image: UIImage?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if let image = try? ImageService.shared.fetch(url: url) {
+            content(.success(image))
+        } else {
+            AsyncImage(url: url) { result in
+                if case .success(let image) = result {
+                    content(.success(image))
+                        .task {
+                            ImageService.shared.save(image: image, at: url)
+                        }
+                } else {
+                    content(result)
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    CachedAsyncImage()
+    CachedAsyncImage(url: nil) { _ in }
 }
